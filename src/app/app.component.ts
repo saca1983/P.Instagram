598e1f00118589dd336f29495383c37cfcbe3940
Angel s <apsaca@sudamericano.edu.ec>
2024-08-04 21:52:20 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Comp1Component } from '../components/comp1/comp1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Comp1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
